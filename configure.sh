e5ec39ac13d3f30ccd38f34f94853ec5910a8219
jldeen <jedeen@outlook.com>
2017-10-25 20:40:37 -0700

    final

--- configure.sh
+++ configure.sh
@@ -28,7 +28,7 @@ fi
 echo ''
 echo "Now installing oh-my-zsh"
 echo ''
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh; exit 0)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # oh-my-zsh plugin install
 echo ''
