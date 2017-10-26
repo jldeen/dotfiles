7aa1e0866ac783e9467013e2ff4f15108850637a
jldeen <jedeen@outlook.com>
2017-10-25 19:41:00 -0700

    added az cli support and prompts

--- configure.sh
+++ configure.sh
@@ -20,7 +20,6 @@ echo "Downloading git-completion for git version: $GIT_VERSION..."
 if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
 	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
 fi
-echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc
 
 # oh-my-zsh install
 echo ''
@@ -35,7 +34,6 @@ echo ''
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 # powerlevel9k install
 echo ''
@@ -84,24 +82,73 @@ mv dircolors.256dark .dircolors
 
 # Pull down personal dotfiles
 echo ''
-echo "Now pulling down jldeen dotfiles..."
-git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
-echo ''
-cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
-echo ''
-echo "Checking out WSL branch..." && git checkout wsl
-echo ''
-echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
+read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo ''
+	echo "Now pulling down jldeen dotfiles..."
+	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
+	echo ''
+	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
+	echo ''
+	echo "Checking out WSL branch..." && git checkout wsl
+	echo ''
+	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully configured your environment with jldeen's dotfiles..."
+    else
+        echo "jldeen's dotfiles were not applied successfully..." >&2
+fi
+else 
+	echo ''
+    echo "You chose not to apply jldeen's dotfiles. You will need to configure your environment manually..."
+	echo ''
+	echo "Setting defaults for .zshrc and .bashrc..."
+	echo ''
+	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
+	echo ''
+	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
+fi
 
 # Setup and configure az cli
 echo ''
+read -p "Do you want to install Azure CLI? y/n" -n 1 -r
 echo "Now installing az cli..."
-curl -L https://aka.ms/InstallAzureCli | bash
-exec -l $SHELL
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get update -y
+    sudo apt-get install python libssl-dev libffi-dev python-dev build-essential -y
+	curl -L https://aka.ms/InstallAzureCli | bash
+	exec -l $SHELL
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed Azure CLI 2.0."
+    else
+        echo "Azure CLI not installed successfully." >&2
+fi
+else 
+    echo "You chose not to install Azure CLI. Exiting now."
+fi
 
 # Set default shell to zsh
 echo ''
-echo "Now setting default shell..."
-chsh -s $(which zsh); exit 0
-
+read -p "Do you want to change your default shell? y/n" -n 1 -r
+echo ''
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Now setting default shell..."
+    chsh -s $(which zsh); exit 0
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed Azure CLI 2.0..."
+    else
+        echo "Azure CLI not installed successfully..." >&2
+fi
+else 
+    echo "You chose not to install Azure CLI. Exiting now..."
+fi
+echo ''
 echo '	Badass WSL terminal installed!'
