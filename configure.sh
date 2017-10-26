6e42bfaddeaaddecef555529bcea9a1332856054
jldeen <jedeen@outlook.com>
2017-10-25 19:02:17 -0700

    Added git-completion

--- configure.sh
+++ configure.sh
@@ -6,6 +6,22 @@ echo "Now installing zsh..."
 echo ''
 sudo apt install zsh -y
 
+# Installing git completion
+echo ''
+echo "Now installing git and bash-completion..."
+sudo apt-get install git bash-completion -y
+
+echo ''
+echo "Now configuring git-completion..."
+GIT_VERSION=`git --version | awk '{print $3}'`
+URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
+echo ''
+echo "Downloading git-completion for git version: $GIT_VERSION..."
+if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
+	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
+fi
+echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc
+
 # oh-my-zsh install
 echo ''
 echo "Now installing oh-my-zsh"
