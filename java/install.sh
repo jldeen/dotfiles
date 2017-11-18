9902dadf4f10cea2b93d3fa2bc256aae1cff98ad
Ronnie Hicks <hicks.ronnie@gmail.com>
2017-11-17 23:29:05 -0600

    Install SDKMAN and NVM

---
+++ java/install.sh
@@ -0,0 +1,4 @@
+if test ! $(which sdk)
+then
+  curl -s "https://get.sdkman.io" | bash
+fi
--- node/install.sh
+++ node/install.sh
@@ -2,3 +2,9 @@ if test ! $(which spoof)
 then
   sudo npm install spoof -g
 fi
+
+# install NVM
+if test ! $(which nvm)
+then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+fi
