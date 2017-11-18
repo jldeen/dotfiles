8266625d66a824b71d902169e2784354f70bc7c7
Ronnie Hicks <hicks.ronnie@gmail.com>
2017-11-18 02:12:51 -0600

    Fixing up some more VIM stuff

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,7 @@
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+
+if [ ! -d "$HOME/.vim/autoload" ]; then
+  sudo curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
