4bb89148ec95f54ff5494cd92cdb77eba1556a69
Ronnie Hicks <hicks.ronnie@gmail.com>
2017-11-24 12:15:27 -0600

    Getting powerlevel9k where i like it

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,5 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias printcolors='for code ({000..255}) print -P -- "$code: %F{$code}This is how your text would look like%f"'
