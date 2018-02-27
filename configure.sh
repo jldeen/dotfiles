a52d5a0b7a93bd8e635b496d97d5ba57287262ea
jldeen <jedeen@outlook.com>
2018-02-27 08:46:01 -0800

    update az cli install

--- configure.sh
+++ configure.sh
@@ -131,9 +131,14 @@ echo ''
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "Now installing az cli..."
-    sudo apt-get install python libssl-dev libffi-dev python-dev build-essential -y
-	curl -L https://aka.ms/InstallAzureCli | bash
-	exec -l $SHELL
+    AZ_REPO=$(lsb_release -cs)
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+    sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+    sudo apt-get install apt-transport-https
+    sudo apt-get update && sudo apt-get install azure-cli
+	
     if [[ $? -eq 0 ]]
     then
         echo "Successfully installed Azure CLI 2.0."
