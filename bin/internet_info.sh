ad7414adf8f4f27f07a4208777eddfe5bf895192
jldeen <jedeen@outlook.com>
2018-02-26 18:59:29 -0800

    Add packet loss and bandwidth info

--- bin/internet_info.sh
+++ bin/internet_info.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-#IP=$(ipconfig getifaddr wifi0)
 IP=$(hostname -I | awk {'print $1}')
-PUB_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+P_L=$(ping google.com -t 4 | grep "packet loss" | awk '{print $7}')
+DL=$(speedtest-cli --simple | awk 'NR==2{print $2}')
+UP=$(speedtest-cli --simple | awk 'NR==3{print $2}')
 
+# # Public IP
+PUB_IP=$(speedtest-cli --json | jq -r .client.ip)
+
+if [[ "$PUB_IP" = ";; connection timed out; no servers could be reached" ]]; then 
+    PUB_IP="Not Available"
+elif [[ "$PUB_IP" = "" ]]; then
+    PUB_IP="No external access"
+else 
+    PUB_IP=$(speedtest-cli --json | jq -r .client.ip)
+fi
+ 
 INTERNET=''
 
 #internet_info=`airport -I | grep agrCtlRSSI | awk '{print $2}' | sed 's/-//g'`
@@ -20,6 +32,4 @@ else
     echo -n '#[fg=colour150]'
 fi
 
-echo -n "#[fg=colour60]$INTERNET  -[$internet_info]db #[fg=colour60]$IP | $PUB_IP"
-
-
+echo -n "#[fg=colour60]$INTERNET  -[$internet_info]db | #[fg=colour81]$P_L p/l #[fg=colour86]$DL Mbit/s $UP Mbit/s #[fg=colour60]$IP | $PUB_IP"
