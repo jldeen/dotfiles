fbf5bbca6e4d9aef15d140bd843b040f9e4ea0ce
jldeen <jedeen@outlook.com>
2018-02-26 21:20:27 -0800

    add in timeout check for pub_ip; clean up

--- bin/internet_info.sh
+++ bin/internet_info.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
+#IP=$(ipconfig getifaddr wifi0)
 IP=$(hostname -I | awk {'print $1}')
-P_L=$(ping google.com -t 4 | grep "packet loss" | awk '{print $7}')
-DL=$(speedtest-cli --simple | awk 'NR==2{print $2}')
-UP=$(speedtest-cli --simple | awk 'NR==3{print $2}')
 
 # # Public IP
-PUB_IP=$(speedtest-cli --json | jq -r .client.ip)
+PUB_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 if [[ "$PUB_IP" = ";; connection timed out; no servers could be reached" ]]; then 
     PUB_IP="Not Available"
 elif [[ "$PUB_IP" = "" ]]; then
     PUB_IP="No external access"
 else 
-    PUB_IP=$(speedtest-cli --json | jq -r .client.ip)
+    PUB_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 fi
- 
-INTERNET=''
 
-#internet_info=`airport -I | grep agrCtlRSSI | awk '{print $2}' | sed 's/-//g'`
+INTERNET=''
 
 if [[ $internet_info -lt 20 ]]; then
     echo -n '#[fg=colour150]'
@@ -32,4 +28,4 @@ else
     echo -n '#[fg=colour150]'
 fi
 
-echo -n "#[fg=colour60]$INTERNET  -[$internet_info]db | #[fg=colour81]$P_L p/l #[fg=colour86]$DL Mbit/s $UP Mbit/s #[fg=colour60]$IP | $PUB_IP"
+echo -n "#[fg=colour60]$INTERNET #[fg=colour60]$IP | $PUB_IP"
