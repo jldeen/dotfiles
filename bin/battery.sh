d88d5c89e350ff957c1b5e5bcfce9690e8351356
jldeen <jedeen@outlook.com>
2018-02-26 23:00:19 -0800

    update bin files for macOS

--- bin/battery.sh
+++ bin/battery.sh
@@ -6,15 +6,15 @@ battery_info=`pmset -g batt`
 current_charge=$(echo $battery_info | grep -o '[0-9]\+%' | awk '{sub (/%/, "", $1); print $1}')
 
 if [[ $current_charge -lt 30 ]]; then
-    echo -n '#[fg=colour31]'
+    echo -n '#[fg=colour41]'
 elif [[ $current_charge -lt 50 ]]; then
-    echo -n '#[fg=colour32]'
+    echo -n '#[fg=colour42]'
 elif [[ $current_charge -lt 70 ]]; then
-    echo -n '#[fg=colour33]'
+    echo -n '#[fg=colour43]'
 elif [[ $current_charge -lt 90 ]]; then
-    echo -n '#[fg=colour34]'
+    echo -n '#[fg=colour44]'
 else
-    echo -n '#[fg=colour35]'
+    echo -n '#[fg=colour45]'
 fi
 
 echo -n "$HEART $current_charge%"
--- bin/internet_info.sh
+++ bin/internet_info.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-#IP=$(ipconfig getifaddr wifi0)
-IP=$(hostname -I | awk {'print $1}')
-PUB_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+IP=$(ipconfig getifaddr en0)
+P_L=$(ping google.com -t 4 | grep "packet loss" | awk '{print $7}')
+DL=$(speedtest-cli --simple | awk 'NR==2{print $2}')
+UP=$(speedtest-cli --simple | awk 'NR==3{print $2}')
 
+# # Public IP
+PUB_IP=$(speedtest-cli --json | jq -r .client.ip)
+
+if [[ "$PUB_IP" = ";; connection timed out; no servers could be reached" ]]; then 
+    PUB_IP="Not Available"
+elif [[ "$PUB_IP" = "" ]]; then
+    PUB_IP="No external access"
+else 
+    PUB_IP=$(speedtest-cli --json | jq -r .client.ip)
+fi
+ 
 INTERNET=''
 
-#internet_info=`airport -I | grep agrCtlRSSI | awk '{print $2}' | sed 's/-//g'`
+internet_info=`airport -I | grep agrCtlRSSI | awk '{print $2}' | sed 's/-//g'`
 
 if [[ $internet_info -lt 20 ]]; then
-    echo -n '#[fg=colour150]'
+    echo -n '#[fg=colour116]'
 elif [[ $internet_info -lt 30 ]]; then
-    echo -n '#[fg=colour155]'
+    echo -n '#[fg=colour117]'
 elif [[ $internet_info -lt 40 ]]; then
-    echo -n '#[fg=colour160]'
+    echo -n '#[fg=colour118]'
 elif [[ $internet_info -lt 50 ]]; then
-    echo -n '#[fg=colour163]'
+    echo -n '#[fg=colour119]'
 else
-    echo -n '#[fg=colour150]'
+    echo -n '#[fg=colour120]'
 fi
 
-echo -n "#[fg=colour60]$INTERNET  -[$internet_info]db #[fg=colour60]$IP | $PUB_IP"
-
-
+echo -n "$INTERNET  -[$internet_info]db | #[fg=colour81]$P_L p/l #[fg=colour86]$DL Mbit/s $UP Mbit/s #[fg=colour197]$IP | $PUB_IP"
