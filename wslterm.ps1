970878f43d64386ab768cf5c1b22434f07f38c5b
Jessica Deen <jldeen@users.noreply.github.com>
2018-03-01 11:30:04 -0800

    Update wslterm.ps1
    test new function

--- wslterm.ps1
+++ wslterm.ps1
@@ -1,3 +1,30 @@
+function Ignore-SelfSignedCerts
+{
+try
+{
+Write-Host "Adding TrustAllCertsPolicy type." -ForegroundColor White
+Add-Type -TypeDefinition @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy
+{
+public bool CheckValidationResult(
+ServicePoint srvPoint, X509Certificate certificate,
+WebRequest request, int certificateProblem)
+{
+return true;
+}
+}
+"@
+Write-Host "TrustAllCertsPolicy type added." -ForegroundColor White
+}
+catch
+{
+Write-Host $_ -ForegroundColor "Yellow"
+}
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
+Ignore-SelfSignedCerts;
 
 # Function to extract our 7z download
 Function Expand-Archive([string]$Path, [string]$Destination, [switch]$RemoveSource) {
