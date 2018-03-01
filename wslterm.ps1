e3b8778ceb47eb9f00ca49922a4936d02f0b659f
Jessica Deen <jldeen@users.noreply.github.com>
2018-03-01 12:01:08 -0800

    Update wslterm.ps1
    Fixed tls method for Invoke-WebRequest & updated symlink creation

--- wslterm.ps1
+++ wslterm.ps1
@@ -1,31 +1,3 @@
-function Ignore-SelfSignedCerts
-{
-try
-{
-Write-Host "Adding TrustAllCertsPolicy type." -ForegroundColor White
-Add-Type -TypeDefinition @"
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-public class TrustAllCertsPolicy : ICertificatePolicy
-{
-public bool CheckValidationResult(
-ServicePoint srvPoint, X509Certificate certificate,
-WebRequest request, int certificateProblem)
-{
-return true;
-}
-}
-"@
-Write-Host "TrustAllCertsPolicy type added." -ForegroundColor White
-}
-catch
-{
-Write-Host $_ -ForegroundColor "Yellow"
-}
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-}
-Ignore-SelfSignedCerts;
-
 # Function to extract our 7z download
 Function Expand-Archive([string]$Path, [string]$Destination, [switch]$RemoveSource) {
 	$7z_Application = "C:\Program Files\7-Zip\7z.exe"
@@ -48,17 +20,13 @@ cd $env:USERPROFILE
 $wslTerminal = "wsl-terminal-0.8.3.7z"
 
 # Get bits for WSL terminal
-wget https://github.com/goreliu/wsl-terminal/releases/download/v0.8.3/wsl-terminal-0.8.3.7z -OutFile $env:USERPROFILE\$wslTerminal
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri 'https://github.com/goreliu/wsl-terminal/releases/download/v0.8.3/wsl-terminal-0.8.3.7z' -OutFile $env:USERPROFILE\$wslTerminal
 
 # Extract WSL terminal and remove after complete
 Get-ChildItem $wslTerminal -Filter *.7z | ForEach-Object {
 	Expand-Archive -Path $_.FullName -Destination $env:USERPROFILE -RemoveSource
 }
 
-# Add shortcut to desktop
-$TargetFile = "$env:USERPROFILE\wsl-terminal\open-wsl.exe"
-$ShortcutFile = "$env:USERPROFILE\Desktop\bash.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.TargetPath = $TargetFile
-$Shortcut.Save()
+# Create symlink between wsl-emulator and desktop
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Desktop\" -Name "wsl.lnk" -Value "$env:USERPROFILE\wsl-terminal\open-wsl.exe"
