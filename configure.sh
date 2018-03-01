96bd274313fe14eff9dd93e46db000896d352a46
joben <joben.rara@gmail.com>
2018-03-01 11:21:51 +0800

    restored configure.sh to previous

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 # Update pkg lists
 
 echo "Updating package lists..."
@@ -31,50 +31,50 @@ echo ''
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # oh-my-zsh plugin install
-#echo ''
-#echo "Now installing oh-my-zsh plugins..."
-#echo ''
-#git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-#git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+echo ''
+echo "Now installing oh-my-zsh plugins..."
+echo ''
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
 # powerlevel9k install
-#echo ''
-#echo "Now installing powerlevel9k..."
-#echo ''
-#git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo ''
+echo "Now installing powerlevel9k..."
+echo ''
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # vimrc vundle install
-#echo ''
-#echo "Now installing vundle..."
-#echo ''
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo ''
+echo "Now installing vundle..."
+echo ''
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Pathogen install
-#echo ''
-#echo "Now installing Pathogen..."
-#echo ''
-#mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-#	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo ''
+echo "Now installing Pathogen..."
+echo ''
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Nerdtree for vim install
-#echo ''
-#echo "Now installing Nerdtree for Vim..."
-#echo ''
-#git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+echo ''
+echo "Now installing Nerdtree for Vim..."
+echo ''
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 # Vim color scheme install
-#echo ''
-#echo "Now installing vim wombat color scheme..."
-#echo ''
-#git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat 
-#mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
+echo ''
+echo "Now installing vim wombat color scheme..."
+echo ''
+git clone https://github.com/sheerun/vim-wombat-scheme.git ~/.vim/colors/wombat 
+mv ~/.vim/colors/wombat/colors/* ~/.vim/colors/
 
 # Midnight commander install
-#echo ''
-#echo "Now installing Midnight commander..."
-#echo ''
-#sudo apt-get install mc -y
+echo ''
+echo "Now installing Midnight commander..."
+echo ''
+sudo apt-get install mc -y
 
 # Bash color scheme
 echo ''
@@ -84,76 +84,76 @@ wget https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolor
 mv dircolors.256dark .dircolors
 
 # Pull down personal dotfiles
-#echo ''
-#read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
-#echo    # (optional) move to a new line
-#if [[ $REPLY =~ ^[Yy]$ ]]
-#then
-#    echo ''
-#	echo "Now pulling down jldeen dotfiles..."
-#	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
-#	echo ''
-#	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
-#	echo ''
-#	echo "Checking out WSL branch..." && git checkout wsl
-#	echo ''
-#	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
-#    if [[ $? -eq 0 ]]
-#    then
-#        echo "Successfully configured your environment with jldeen's dotfiles..."
-#    else
-#        echo "jldeen's dotfiles were not applied successfully..." >&2
-#fi
-#else 
-#	echo ''
-#    echo "You chose not to apply jldeen's dotfiles. You will need to configure your environment manually..."
-#	echo ''
-#	echo "Setting defaults for .zshrc and .bashrc..."
-#	echo ''
-#	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
-#	echo ''
-#	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
-#	echo ''
-#	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
+echo ''
+read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo ''
+	echo "Now pulling down jldeen dotfiles..."
+	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
+	echo ''
+	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
+	echo ''
+	echo "Checking out WSL branch..." && git checkout wsl
+	echo ''
+	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully configured your environment with jldeen's dotfiles..."
+    else
+        echo "jldeen's dotfiles were not applied successfully..." >&2
+    fi
+else 
+	echo ''
+        echo "You chose not to apply jldeen's dotfiles. You will need to configure your environment manually..."
+	echo ''
+	echo "Setting defaults for .zshrc and .bashrc..."
+	echo ''
+	echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-syntax-highlighting to .zshrc..."
+	echo ''
+	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
+	echo ''
+	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
 	
-#fi
+fi
 
 # Setup and configure az cli
-#echo ''
-#read -p "Do you want to install Azure CLI? y/n (This will take some time...)" -n 1 -r
-#echo ''
-#if [[ $REPLY =~ ^[Yy]$ ]]
-#then
-#	echo "Now installing az cli..."
-#    sudo apt-get install python libssl-dev libffi-dev python-dev build-essential -y
-#	curl -L https://aka.ms/InstallAzureCli | bash
-#	exec -l $SHELL
-#    if [[ $? -eq 0 ]]
-#    then
-#        echo "Successfully installed Azure CLI 2.0."
-#    else
-#        echo "Azure CLI not installed successfully." >&2
-#fi
-#else 
-#    echo "You chose not to install Azure CLI. Exiting now."
-#fi
+echo ''
+read -p "Do you want to install Azure CLI? y/n (This will take some time...)" -n 1 -r
+echo ''
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Now installing az cli..."
+    sudo apt-get install python libssl-dev libffi-dev python-dev build-essential -y
+	curl -L https://aka.ms/InstallAzureCli | bash
+	exec -l $SHELL
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully installed Azure CLI 2.0."
+    else
+        echo "Azure CLI not installed successfully." >&2
+fi
+else 
+    echo "You chose not to install Azure CLI. Exiting now."
+fi
 
 # Set default shell to zsh
-#echo ''
-#read -p "Do you want to change your default shell? y/n" -n 1 -r
-#echo ''
-#if [[ $REPLY =~ ^[Yy]$ ]]
-#then
-#	echo "Now setting default shell..."
-#    chsh -s $(which zsh); exit 0
-#    if [[ $? -eq 0 ]]
-#    then
-#        echo "Successfully set your default shell to zsh..."
-#    else
-#        echo "Default shell not set successfully..." >&2
-#fi
-#else 
-#    echo "You chose not to set your default shell to zsh. Exiting now..."
-#fi
-#echo ''
-#echo '	Badass WSL terminal installed!'
+echo ''
+read -p "Do you want to change your default shell? y/n" -n 1 -r
+echo ''
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Now setting default shell..."
+    chsh -s $(which zsh); exit 0
+    if [[ $? -eq 0 ]]
+    then
+        echo "Successfully set your default shell to zsh..."
+    else
+        echo "Default shell not set successfully..." >&2
+    fi
+else 
+    echo "You chose not to set your default shell to zsh. Exiting now..."
+fi
+echo ''
+echo '	Badass WSL terminal installed!'
