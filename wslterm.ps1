f9d60f747347fa904a169be708da1acb93919131
jldeen <jedeen@outlook.com>
2018-03-01 13:32:05 -0800

    Update symlink creation

--- wslterm.ps1
+++ wslterm.ps1
@@ -1,31 +1,21 @@
-# Check for 7z install
-$7z = 'C:\Program Files\7-Zip\7z.e'
-if (-NOT (Test-Path $7z)) {
-	echo "7zip not found, installing now..."
-	install7z
-	}
-
+# Silent Install 7-Zip
+# http://www.7-zip.org/download.html
+# https://forum.pulseway.com/topic/1939-install-7-zip-with-powershell/ 
 Function install7z {
-	# Silent Install 7-Zip
-	# http://www.7-zip.org/download.html 
-
 	# Path for the workdir
 	$workdir = "c:\installer\"
 
 	# Check if work directory exists if not create it
-
 	If (Test-Path -Path $workdir -PathType Container)
 	{ Write-Host "$workdir already exists" -ForegroundColor Red}
 	ELSE
 	{ New-Item -Path $workdir  -ItemType directory }
 
 	# Download the installer
-
 	$source = "http://www.7-zip.org/a/7z1801-x64.msi"
 	$destination = "$workdir\7-Zip.msi"
 
 	# Check if Invoke-Webrequest exists otherwise execute WebClient
-
 	if (Get-Command 'Invoke-Webrequest')
 	{
 		Invoke-WebRequest $source -OutFile $destination
@@ -39,17 +29,20 @@ Function install7z {
 	Invoke-WebRequest $source -OutFile $destination 
 
 	# Start the installation
-
 	msiexec.exe /i "$workdir\7-Zip.msi" /qb
-
 	# Wait XX Seconds for the installation to finish
-
 	Start-Sleep -s 35
 
 	# Remove the installer
-
 	rm -Force $workdir\7*
 }
+
+# Check for 7z install
+$7z = 'C:\Program Files\7-Zip\7z.e'
+if (-NOT (Test-Path $7z)) {
+	Write-Host "7zip not found, installing now..."
+	install7z
+	}
 
 # Function to extract our 7z download
 Function Expand-Archive([string]$Path, [string]$Destination, [switch]$RemoveSource) {
@@ -81,5 +74,9 @@ Get-ChildItem $wslTerminal -Filter *.7z | ForEach-Object {
 	Expand-Archive -Path $_.FullName -Destination $env:USERPROFILE -RemoveSource
 }
 
-# Create symlink between wsl-emulator and desktop
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Desktop\" -Name "wsl.lnk" -Value "$env:USERPROFILE\wsl-terminal\open-wsl.exe"
+# Check if symlink exists. If not, create it
+$symlink = "$env:USERPROFILE\Desktop\wsl.lnk"
+If (Test-Path -Path $symlink)
+	{ Write-Host "$symlink already exists" -ForegroundColor Red }
+ELSE
+	{ New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\Desktop\" -Name "wsl.lnk" -Value "$env:USERPROFILE\wsl-terminal\open-wsl.exe" }
