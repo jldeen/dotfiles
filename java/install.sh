8316ac4d4abe9499f077d88ea593053612d845fe
Ronnie Hicks <ronnie@Ronnies-MBP.hicks.local>
2018-03-11 22:09:15 -0500

    Echo and update permissions

--- java/install.sh
+++ java/install.sh
@@ -1,3 +1,4 @@
 if [ ! -d "$HOME/.sdkman" ]; then
+  echo "› installing sdkman"
   sudo curl -s "https://get.sdkman.io" | bash
 fi
--- vim/install.sh
+++ vim/install.sh
@@ -1,8 +1,10 @@
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+  echo "› installing vundle"
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 if [ ! -x "$HOME/.vimrc" ]; then
+  echo "› installing vim plugins"
   vim +PluginInstall +qall
 fi
 
