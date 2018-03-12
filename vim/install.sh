0c1f11cbe5709811286bd08d721e492d9b730332
Ronnie Hicks <hicks.ronnie@gmail.com>
2018-03-11 23:47:26 -0500

    Fuck it

--- vim/install.sh
+++ vim/install.sh
@@ -3,11 +3,6 @@ if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
-if [ ! -x "$HOME/.vimrc" ]; then
-  echo "› installing vim plugins"
-  vim +PluginInstall +qall || true
-fi
-
 # if [ ! -d "$HOME/.vim/autoload" ]; then
 #   mkdir ~/.vim/autoload
 #   sudo curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
