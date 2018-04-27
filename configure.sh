c5ffe440b3103206a383bcc46266a333dc26219d
Jerry Locke <jlocke789@gmail.com>
2018-04-27 09:23:32 -0500

    Add new signing key for apt

--- configure.sh
+++ configure.sh
@@ -138,6 +138,7 @@ then
      sudo tee /etc/apt/sources.list.d/azure-cli.list
 
     sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+    sudo curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
     sudo apt-get install apt-transport-https
     sudo apt-get update && sudo apt-get install azure-cli
 	
