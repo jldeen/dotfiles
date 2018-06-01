e41686a52467ab905eaffba3b1797f8d7385d63a
jldeen <jedeen@outlook.com>
2018-06-01 08:47:34 -0700

    Testing removal of repeate promp for default shell

--- configure.sh
+++ configure.sh
@@ -138,7 +138,7 @@ then
     echo "Running speedtest-cli..."
     speedtest-cli --simple > ~/bin/bandwidth.log
     speedtest-cli --json > ~/bin/bandwidth.json
-    
+
 
     if [[ $? -eq 0 ]]
     then
@@ -161,40 +161,22 @@ else
 fi
 
 # Set default shell to zsh
-echo ''
-read -p "Do you want to change your default shell? y/n" -n 1 -r
-echo ''
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo "Now setting default shell..."
-    chsh -s $(which zsh)
-    if [[ $? -eq 0 ]]
-    then
-        echo "Successfully set your default shell to zsh..."
-    else
-        echo "Default shell not set successfully..." >&2
-fi
-else 
-    echo "You chose not to set your default shell to zsh. Exiting now..."
-fi
-
-# Setup and configure az cli
-echo ''
-read -p "Do you want to install Azure CLI? y/n (This will take some time...)" -n 1 -r
-echo ''
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo "Now installing az cli..."
-    brew install azure-cli
-    if [[ $? -eq 0 ]]
-    then
-        echo "Successfully installed Azure CLI 2.0."
-    else
-        echo "Azure CLI not installed successfully." >&2
-fi
-else 
-    echo "You chose not to install Azure CLI. Exiting now."
-fi
+# echo ''
+# read -p "Do you want to change your default shell? y/n" -n 1 -r
+# echo ''
+# if [[ $REPLY =~ ^[Yy]$ ]]
+# then
+# 	echo "Now setting default shell..."
+#     chsh -s $(which zsh)
+#     if [[ $? -eq 0 ]]
+#     then
+#         echo "Successfully set your default shell to zsh..."
+#     else
+#         echo "Default shell not set successfully..." >&2
+# fi
+# else 
+#     echo "You chose not to set your default shell to zsh. Exiting now..."
+# fi
 
 echo ''
 echo '	Badass macOS terminal installed!'
