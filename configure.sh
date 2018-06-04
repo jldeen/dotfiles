9de35363079182ea539dd8cdc795a0d5ef84d1cc
jldeen <jedeen@outlook.com>
2018-06-04 15:59:10 -0700

    Prep for merge, update branch refs

--- configure.sh
+++ configure.sh
@@ -124,7 +124,7 @@ then
 	echo ''
 	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
 	echo ''
-	echo "Checking out macOS dev branch..." && git checkout mac-dev
+	echo "Checking out macOS branch..." && git checkout mac
 	echo ''
 	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
     echo ''
