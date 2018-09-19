1fa0e5d7fab881d20993cbebc5d560b12acd599a
Ronnie Hicks <rohicks@lumeris.com>
2018-09-19 15:50:22 -0500

    Removed default_user and adding some more plugins

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,13 @@
+CUSTOM_DIR = ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
 echo "› re-installing powerlevel9k theme"
-rm -rf ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+rm -rf ${CUSTOM_DIR}/themes/powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ${CUSTOM_DIR}/themes/powerlevel9k
+
+echo "› re-installing zsh-autosuggestions"
+rm -rf ${CUSTOM_DIR}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${CUSTOM_DIR}/plugins/zsh-autosuggestions
+
+echo "› re-installing zsh-syntax-highlighting"
+rm -rf ${CUSTOM_DIR}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${CUSTOM_DIR}/plugins/zsh-syntax-highlighting
