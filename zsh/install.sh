e8eb7101daf8afb5849736517f990b382f656808
Ronnie Hicks <rohicks@lumeris.com>
2018-09-27 15:43:51 -0500

    Force re-install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,13 +1,13 @@
 CUSTOM_DIR = ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
 echo "› re-installing powerlevel9k theme"
-rm -rf ${CUSTOM_DIR}/themes/powerlevel9k
+sudo rm -rf ${CUSTOM_DIR}/themes/powerlevel9k
 sudo git clone https://github.com/bhilburn/powerlevel9k.git ${CUSTOM_DIR}/themes/powerlevel9k
 
 echo "› re-installing zsh-autosuggestions"
-rm -rf ${CUSTOM_DIR}/plugins/zsh-autosuggestions
+sudo rm -rf ${CUSTOM_DIR}/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${CUSTOM_DIR}/plugins/zsh-autosuggestions
 
 echo "› re-installing zsh-syntax-highlighting"
-rm -rf ${CUSTOM_DIR}/plugins/zsh-syntax-highlighting
+sudo rm -rf ${CUSTOM_DIR}/plugins/zsh-syntax-highlighting
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${CUSTOM_DIR}/plugins/zsh-syntax-highlighting
